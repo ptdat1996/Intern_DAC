0f8d50497648b5e2ed567e7fc39d23fc7ee5d22b
ptdat1996 <kidgiangho1996@gmail.com>
2018-12-24 11:46:50 +0700

    change menu item color when scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*------change header color and hide/show back to top button when scroll----*/
+//#region change header color and hide/show back to top button when scroll
 $(".back-to-top").hide();
 $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
@@ -9,9 +9,10 @@ $(window).scroll(function () {
         $('.back-to-top').fadeOut('slow');
     }
 });
+//#endregion 
 
-/*------handle event back to top----*/
-function backToTop(){
+//#region handle event back to top
+function backToTop() {
     $("body,html").animate({
         scrollTop: 0
     }, 800);
@@ -24,8 +25,9 @@ $(".back-to-top").click(function () {
     navMenu.children().first().addClass("menu-active");
     return false;
 });
+//#endregion
 
-/*-------- loading skill when scroll to-----*/
+//#region loading skill when scroll to it
 $(function () {
     var position = $(".skills").offset().top;
 
@@ -41,10 +43,11 @@ $(function () {
         }
     });
 });
+//#endregion
 
-/*-----------handling event scroll to the part of the page-----------*/
+//#region handling event scroll to the part of the page
 function goToByScroll(className) {
-    if(className === "home"){
+    if (className === "home") {
         backToTop();
         return false;
     }
@@ -54,23 +57,31 @@ function goToByScroll(className) {
     }, 1500);
 }
 
-$(".nav-menu a").on("click", function () {
-    // Call the scroll function
-    goToByScroll($(this).attr("class"));
+$(".nav-menu a").on("click", function (e) {
+    if ($(this).attr("class") != undefined) {
+        // Call the scroll function
+        e.preventDefault();
+        goToByScroll($(this).attr("class"));
+    }
 });
 
-$(".footer-links a").on("click", function () {
-    // Call the scroll function
-    goToByScroll($(this).attr("class"));
+$(".footer-links a").on("click", function (e) {
+    e.preventDefault();
+    if ($(this).attr("class") != undefined) {
+        // Call the scroll function
+        goToByScroll($(this).attr("class"));
+    }
 });
+//#endregion
 
-/*---------------change color menu when click on it----------------*/
+//#region change color menu when click on it
 $(".nav-menu a").click(function () {
     $(".nav-menu .menu-active").removeClass("menu-active");
     $(this).closest("li").addClass("menu-active");
 });
+//#endregion
 
-/*------------------button mobile navbar toggle---------------------*/
+//#region button mobile navbar toggle
 $(".mobile-nav-toggle").click(function () {
     $(".mobile-nav-menu-container").css("width", "250px");
 });
@@ -78,8 +89,9 @@ $(".mobile-nav-toggle").click(function () {
 $(".btn-close").click(function () {
     $(".mobile-nav-menu-container").css("width", "0");
 });
+//#endregion
 
-/*-------------------mobile menu click event handle-----------------*/
+//#region mobile menu click event handle
 $(".mobile-nav-menu a").click(function () {
     $(".mobile-nav-menu-container").css("width", "0");
     goToByScroll($(this).attr("class"));
@@ -101,5 +113,40 @@ $("body").click(function (e) {
         }
     }
 });
+//#endregion
 
-/*--------------------------change menu active when scroll to part of website-------------------------*/
+//#region change menu active when scroll to part of website
+$(function () {
+    var aboutSection = $(".about").offset().top;
+    var serviceSection = $(".service").offset().top;
+    var teamSection = $(".team").offset().top;
+    var contactSection = $(".contact").offset().top;
+
+    var nav_item = $(".nav-menu li");
+
+    $(document).scroll(function () {
+        var scrollPosition = $(document).scrollTop();
+        if (scrollPosition < aboutSection) {
+            nav_item.removeClass("menu-active");
+            nav_item.eq(0).addClass("menu-active");
+        }
+        if (scrollPosition >= aboutSection && scrollPosition < serviceSection) {
+            nav_item.removeClass("menu-active");
+            nav_item.eq(1).addClass("menu-active");
+        }
+        else if (scrollPosition >= serviceSection && scrollPosition < teamSection) {
+            nav_item.removeClass("menu-active");
+            nav_item.eq(2).addClass("menu-active");
+        }
+        else if (scrollPosition >= teamSection && scrollPosition < contactSection) {
+            nav_item.removeClass("menu-active");
+            nav_item.eq(3).addClass("menu-active");
+        }
+        else if (scrollPosition >= contactSection) {
+            nav_item.removeClass("menu-active");
+            nav_item.eq(4).addClass("menu-active");
+        }
+
+    });
+});
+//#endregion
