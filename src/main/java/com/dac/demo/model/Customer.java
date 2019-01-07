de3b04a06a5e519e25c83460de39be12c7594c59
ptdat1996 <kidgiangho1996@gmail.com>
2019-01-07 17:00:53 +0700

    add model

---
+++ src/main/java/com/dac/demo/model/Customer.java
@@ -0,0 +1,37 @@
+package com.dac.demo.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Customer implements Serializable {
+
+    private int id;
+    private String name;
+    private LocalDate dob;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+}
