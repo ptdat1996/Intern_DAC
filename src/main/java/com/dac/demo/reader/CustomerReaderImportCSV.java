df061c44383d5a5c3917de4fd376c775aa307c29
ptdat1996 <kidgiangho1996@gmail.com>
2019-01-07 17:03:43 +0700

    add reader csv class

---
+++ src/main/java/com/dac/demo/reader/CustomerReaderImportCSV.java
@@ -0,0 +1,44 @@
+package com.dac.demo.reader;
+
+import com.dac.demo.model.Customer;
+import com.dac.demo.util.AbsReader;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.file.transform.FieldSet;
+
+import java.time.LocalDate;
+
+
+public class CustomerReaderImportCSV extends AbsReader<Customer> {
+
+    private final Logger log = LoggerFactory.getLogger(CustomerReaderImportCSV.class);
+
+
+    @Override
+    protected Customer mapField(FieldSet fieldSet) {
+        log.info("FieldSet:{} ", fieldSet);
+        if (fieldSet == null)
+            return null;
+        Customer customer = new Customer();
+        customer.setName(fieldSet.readString("name"));
+        customer.setDob(convertToDate(fieldSet.readString("dob")));
+        return customer;
+    }
+
+    @Override
+    protected String[] getFieldsName() {
+        return new String[]{"name", "dob"};
+    }
+
+    @Override
+    protected String getDelimiter() {
+        return DelimitedLineTokenizer.DELIMITER_COMMA;
+    }
+
+    private LocalDate convertToDate(String input) {
+        String strSplit[] = input.split("-");
+        LocalDate date = LocalDate.of(Integer.parseInt(strSplit[2]), Integer.parseInt(strSplit[1]), Integer.parseInt(strSplit[0]));
+        return date;
+    }
+}
