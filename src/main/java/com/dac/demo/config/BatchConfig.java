263f71a526d108226efa878040bf16c4f25d898c
ptdat1996 <kidgiangho1996@gmail.com>
2019-01-08 15:34:11 +0700

    add config to run job

---
+++ src/main/java/com/dac/demo/config/BatchConfig.java
@@ -0,0 +1,66 @@
+package com.dac.demo.config;
+
+import com.dac.demo.model.Customer;
+import com.dac.demo.model.CustomerResult;
+import com.dac.demo.processor.CustomerProcessor;
+import com.dac.demo.reader.CustomerReader;
+import com.dac.demo.writer.CustomerWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(BatchConfig.class);
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job exportJob(){
+        return jobBuilderFactory.get("exportJob")
+                .incrementer(new RunIdIncrementer())
+                .flow(step())
+                .end()
+                .build();
+    }
+    @Bean
+    public Step step(){
+        return stepBuilderFactory.get("step")
+                .<Customer, CustomerResult>chunk(10)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+    @Bean(destroyMethod = "")
+    public ItemReader<Customer> reader(){
+        return new CustomerReader().reader();
+    }
+
+    @Bean
+    public CustomerProcessor processor(){
+        return new CustomerProcessor();
+    }
+
+    @Bean(destroyMethod = "")
+    public ItemWriter<CustomerResult> writer(){
+        return new CustomerWriter().writer();
+    }
+
+}
