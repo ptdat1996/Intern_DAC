dfe24376246806632a4850490d7726359e3853fa
ptdat1996 <kidgiangho1996@gmail.com>
2019-01-08 15:32:56 +0700

    add writer class to write csv file

---
+++ src/main/java/com/dac/demo/writer/CustomerWriter.java
@@ -0,0 +1,37 @@
+package com.dac.demo.writer;
+
+import com.dac.demo.model.CustomerResult;
+import com.dac.demo.util.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.core.io.FileUrlResource;
+
+import java.net.MalformedURLException;
+
+public class CustomerWriter {
+
+    private final Logger log = LoggerFactory.getLogger(CustomerWriter.class);
+
+    public FlatFileItemWriter<CustomerResult> writer(){
+        log.info("Begin write");
+        FlatFileItemWriter<CustomerResult> writer = new FlatFileItemWriter<>();
+        try {
+            writer.setResource(new FileUrlResource(Constant.CUSTOMER_DATA_EXPORT_FILE));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        writer.setAppendAllowed(true);
+        DelimitedLineAggregator<CustomerResult> aggregator = new DelimitedLineAggregator<>();
+        aggregator.setDelimiter(DelimitedLineTokenizer.DELIMITER_COMMA);
+        BeanWrapperFieldExtractor<CustomerResult> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[]{"id","name","age"});
+        aggregator.setFieldExtractor(fieldExtractor);
+        writer.setLineAggregator(aggregator);
+        writer.setHeaderCallback(writer1 -> writer1.write("id,name,age"));
+        return writer;
+    }
+}
