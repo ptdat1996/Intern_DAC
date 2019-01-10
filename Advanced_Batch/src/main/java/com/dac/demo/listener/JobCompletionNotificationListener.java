9837e289dc3b29a01032672b389d36cb1d5d0d5c
ptdat1996 <kidgiangho1996@gmail.com>
2019-01-10 13:42:21 +0700

    re-up Advanced_Batch

---
+++ Advanced_Batch/src/main/java/com/dac/demo/listener/JobCompletionNotificationListener.java
@@ -0,0 +1,32 @@
+package com.dac.demo.listener;
+
+import com.dac.demo.util.ResourceConstant;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            int size = jdbcTemplate.queryForList(ResourceConstant.GET_CUSTOMER).size();
+            log.info("Found <" + size + "> in database");
+        }
+    }
+}
---
+++ Advanced_Batch/src/main/java/com/dac/demo/model/Customer.java
@@ -0,0 +1,36 @@
+package com.dac.demo.model;
+
+import java.time.LocalDate;
+
+public class Customer  {
+
+    private int id;
+    private String name;
+    private LocalDate dob;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+}
