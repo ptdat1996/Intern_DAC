44c2e76f88ba999b9c04ab6dc4c6656bf0ebdda7
ptdat1996 <kidgiangho1996@gmail.com>
2019-01-10 13:30:46 +0700

    delete project

--- Advanced_Batch/src/main/java/com/dac/demo/util/AbsWriter.java
+++
@@ -1,7 +0,0 @@
-package com.dac.demo.util;
-
-import org.springframework.batch.item.ItemWriter;
-
-public abstract class AbsWriter<T> implements ItemWriter<T> {
-    public abstract String getSQLString();
-}
--- Advanced_Batch/src/main/resources/schema-mysql.sql
+++
@@ -1,6 +0,0 @@
-USE springbatch_advanced;
-CREATE TABLE customers IF NOT EXISTS  (
-    id INT(11) IDENTITY NOT NULL PRIMARY KEY,
-    name NVARCHAR(45),
-    date_of_birth DATE
-)
